fa47dfc9602a0c9505b6c26b9588f92fba1bffd3
Hayleigh Pervorse <hayleigh_pervorse@redlands.edu>
2018-06-01 08:29:16 -0700

    Css, JS and HTML changes
    added media queries for smaller screen sizes in CSS file,  add pop-up window for alternatives map,

--- js/website.js
+++ js/website.js
@@ -1,14 +1,22 @@
 
 $('#flashMessage').hide();
 
+// test that the message is hidden before clicking submit
+var flashTest1= $("#flashMessage").is(':hidden');
+console.log(flashTest1)
+
 $('#submitbtn').click(function(){
   $('#flashMessage')
     .slideDown(1000)
     .delay(3000)
     .slideUp();
-    document.getElementById("labelhappy").innerHTML = information
+    // document.getElementById("labelhappy").innerHTML = information
     var selValue = $('input[name=optradio]:checked').val();
 });
+
+// test that the message is shown after clicking submit
+var flashTest2= $("#flashMessage").is(':hidden');
+console.log(flashTest2);
 
 var form = document.getElementById("myForm");
 
@@ -33,4 +41,13 @@ function getComment() {
   return  document.getElementById("questionComment");
 }
 
-console.log(document.getElementById("labelhappy"))
+function popup(url) {
+  newwindow=window.open(url,'name','height=600,width=600');
+  if (window.focus) {newwindow.focus()}
+  return false;
+}
+
+var popUp = onclick="return popup('img/Alt1_5.jpg')";
+if (popUp == null || typeof(popUp)=='undefined') {
+	alert('Please disable your pop-up blocker and click the "Open" link again.');
+}
